b177cdd50ac5a296d4546c0cbd89a3ce0d677969
badlydr4wn <axbenz10@gmail.com>
2024-01-31 09:41:21 +1000

    imported new applications

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from textual.app import *
-from environment import EnvironmentApp
+from system.environment import EnvironmentApp
+from system.file_system import FileSystemApp
+# from system.terminal import TerminalApp
 
 if __name__ == "__main__":
     EnvironmentApp().run()
