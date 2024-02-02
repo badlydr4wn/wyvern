986880aa94728f25dbcb5e8d47bf554921a4934f
badlydr4wn <axbenz10@gmail.com>
2024-02-02 19:30:33 +1000

    i am going to fucking kill the pycache

--- system/sysloader.py
+++ system/sysloader.py
@@ -1,10 +1,12 @@
 from textual import *
 from environment import EnvironmentApp
 
-APPS = [EnvironmentApp]
+APPS = [EnvironmentApp()]
 
 def load(application_index):
     APPS[application_index].run()
 
 def exit(application_index):
     APPS[application_index].exit()
+
+load(0)
