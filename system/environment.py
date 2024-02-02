5d50d417206d7363df91bb6859d9be4406ec269f
badlydr4wn <axbenz10@gmail.com>
2024-02-02 13:47:48 +1000

    events take 2 (it works!!!)

--- system/environment.py
+++ system/environment.py
@@ -35,7 +35,7 @@ class EnvironmentApp(App):
         self.title = "Environment"
 
     # Define event functions
-    def on_option_list_option_selected(self, event: OptionList.OptionSelected()):
-        self.notify("yo",title="notif")
+    def on_option_list_option_selected(self, event: OptionList.OptionSelected):
+        self.notify(f"yo - {event.option_index}", title="notif")
         
     # Define keybind functions
